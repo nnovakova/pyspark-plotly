2fd41f9195249360e54e912c2edb706da64c86ad
Nadiia Novakova <nadiia.novakova@gmail.com>
2020-11-01 10:12:54 +0100

    minor refactoring

--- loan-vs-price.py
+++ loan-vs-price.py
@@ -1,11 +1,8 @@
 
 import pyspark
 import datetime
-import chart_studio.plotly as py
 import plotly.graph_objs as go
-import pandas as pd
 from datetime import datetime
-from sklearn import preprocessing
 from plotly.offline import plot
 from pyspark.sql import SparkSession, SQLContext
 from pyspark.sql.types import FloatType, DateType, StructType, StructField
@@ -33,11 +30,9 @@ loanDf = spark.sparkContext \
     .textFile("data.csv") \
     .zipWithIndex() \
     .filter(lambda x: x[1] > 5) \
-    .map(lambda x: x[0]) \
-    .map(lambda x: x.split(',')) \
+    .map(lambda x: x[0].split(',')) \
     .map(lambda x: (datetime.strptime(x[0], '%Y%b'), float(x[4]))) \
     .toDF(loanSchema)
-loanDf.createOrReplaceTempView("loan")
 loanDf.show()
 loanDf.printSchema()
 
@@ -48,11 +43,11 @@ priceDf = spark.read.format("csv").option("header", True) \
     .schema(priceSchema) \
     .load("QDEN628BIS.csv") \
     .select(to_date(col("quarterDate")).alias("quarterDate"), col("index2010"))
-
-priceDf.createOrReplaceTempView("price")
 priceDf.show()
 priceDf.printSchema()
 
+priceDf.createOrReplaceTempView("price")
+loanDf.createOrReplaceTempView("loan")
 joined = spark.sql(
     "select p.quarterDate, l.percent, p.index2010 from price p inner join loan l on p.quarterDate = l.quarterDate order by p.quarterDate") \
     .toPandas()
@@ -61,7 +56,11 @@ print(joined)
 percentValues = normalize(joined, "percent")
 indexValues = normalize(joined, "index2010")
 
-data = [go.Scatter(x=joined.quarterDate, y=percentValues, name="% Loan for House Purchase", text=joined.percent),
-        go.Scatter(x=joined.quarterDate, y=indexValues, name="Residential Property Price (quarterly)", text=joined.index2010)]
+data = [
+    go.Scatter(x=joined.quarterDate, y=percentValues,
+               name="% Loan for House Purchase", text=joined.percent),
+    go.Scatter(x=joined.quarterDate, y=indexValues,
+               name="Residential Property Price (quarterly)", text=joined.index2010)
+]
 fig = go.Figure(data, layout_title_text="Loan vs. Property Price")
 plot(fig, filename='plot.html')
